296dc3f9187b219af89fa8777eda048019a7e830
David Glasser <glasser@davidglasser.net>
2008-12-10 19:29:32 -0800

    Add a TagFamily model and a template which just includes the username.

--- main.py
+++ main.py
@@ -1,37 +1,33 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+#!/usr/bin/env python2.5
 
-
-
-
+import os
 import wsgiref.handlers
 
-
+from google.appengine.api import users
+from google.appengine.ext import db
 from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
 
 
-class MainHandler(webapp.RequestHandler):
+class TagFamily(db.Model):
+  name = db.StringProperty()
+  options = db.StringListProperty()
+
+
+class FamilyHandler(webapp.RequestHandler):
+
+  def render_template(self, template_name, params):
+    path = os.path.join(os.path.dirname(__file__), 'templates',
+                        '%s.html' % template_name)
+    params['current_user'] = users.get_current_user()
+    self.response.out.write(template.render(path, params))
 
   def get(self):
-    self.response.out.write('Hello world!')
+    self.render_template("families", {})
 
 
 def main():
-  application = webapp.WSGIApplication([('/', MainHandler)],
+  application = webapp.WSGIApplication([('/family', FamilyHandler)],
                                        debug=True)
   wsgiref.handlers.CGIHandler().run(application)
 
